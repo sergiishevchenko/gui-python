b5790626b55385aba1bc6836e9be8bf258433ca1
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-10-07 09:19:43 +0200

    add def browse_folder

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import sys
 from PyQt5 import QtWidgets
+import os
 import design
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+    def browse_folder(self):
+        self.listWidget.clear()
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+
+        if directory:
+            for file_name in os.listdir(directory):
+                self.listWidget.addItem(file_name)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
